5b564212678d86ca72ae58632e60f3617d974c7a
georgemirandajr <georgemirandajr@gmail.com>
2023-04-12 19:49:36 -0700

    Update analysis_tab.R

--- R/analysis_tab.R
+++ R/analysis_tab.R
@@ -7,14 +7,8 @@ the Word Press admin site. It provides you a preview of what the data looks like
            after it has been formatted and some helpful stats about the career paths people have created."),
     shiny::tags$h4("Export Data from Word Press"),
     shiny::tags$h6("You must have login credentials to download user data from the Word Press
-           admin site. Assuming you have access, you can download all user data by
-           clicking the button shown below."),
-    shiny::tags$img( src = fs::path_package(
-      "inst", 
-      "wp-admin-export.png", 
-      package = "careerpathfinder"
-    ), 
-    height = "400px"),
+           admin site. Assuming you have access, you can download all user data since inception and
+                   upload it in this app for analysis using the Format Data tab."),
     shiny::tags$br()
   )
 }
